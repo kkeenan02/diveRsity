a2bdd8390bc1c336d290f4f475ffefb162d3be91
kkeenan02 <kkeenan02@qub.ac.uk>
2014-01-28 11:48:58 +0000

    vrsn update (1.7.7)

--- R/diveRsity-code.R
+++ R/diveRsity-code.R
@@ -2,7 +2,7 @@
 NULL
 ################################################################################
 ################################################################################
-##                              diveRsity v1.7.6                              ##  
+##                              diveRsity v1.7.7                              ##  
 ##                            by Kevin Keenan QUB                             ##  
 ##            An R package for the calculation of differentiation             ##
 ##              statistics and locus informativeness statistics               ##  
