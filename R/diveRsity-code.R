df5f0b357240a5195be1a5ed2d3147887676a48f
kkeenan02 <kkeenan02@qub.ac.uk>
2014-02-05 14:55:11 +0000

    v1.8.0 update

--- R/diveRsity-code.R
+++ R/diveRsity-code.R
@@ -16,7 +16,7 @@ divPart <- function(infile = NULL, outfile = NULL, gp = 3, pairwise = FALSE,
                     WC_Fst = FALSE, bs_locus = FALSE, bs_pairwise = FALSE, 
                     bootstraps = 0, plot = FALSE, parallel = FALSE){
   ############################ Argument definitions ############################
-  .Deprecated(new = "fastDivPart", msg = "This function name is no longer in use. Please use 'divPart' instead, \nSee ?fastDivPart for usage details.", 
+  .Deprecated(new = "fastDivPart", msg = "This function name is no longer in use. Please use 'fastDivPart' instead, \nSee ?fastDivPart for usage details.", 
               old = "divPart")
 }
 # divPart<-function(infile = NULL, outfile = NULL, gp = 3, pairwise = FALSE,
