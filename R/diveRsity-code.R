54ac4ae3a56c69a15dab544425f5ad8429aaa402
kkeenan02 <kkeenan02@qub.ac.uk>
2014-02-12 21:23:23 +0000

    horiz genepop allowed

--- R/diveRsity-code.R
+++ R/diveRsity-code.R
@@ -3201,21 +3201,39 @@ pre.divLowMemory <- function(y){
     fileReader <- function (infile) {
       if (typeof(infile) == "list") {
         return(infile)
-      }
-      else if (typeof(infile) == "character") {
+      } else if (typeof(infile) == "character") {
         flForm <- strsplit(infile, split = "\\.")[[1]]
         ext <- flForm[[length(flForm)]]
         if (ext == "arp") {
-          arp2gen(infile)
-          cat("Arlequin file converted to genepop format! \n")
-          infile <- paste(flForm[1], ".gen", sep = "")
+          convRes <- arp2gen(infile)
+          if (!is.null(convRes)) {
+            cat("Arlequin file converted to genepop format! \n")
+            infile <- paste(flForm[1], ".gen", sep = "")
+          } else {
+            infile <- paste(flForm[1], ".gen", sep = "")
+          }
         }
         dat <- scan(infile, sep = "\n", what = "character", quiet = TRUE)
+        if(length(strsplit(dat[4], split = "\\s+")[[1]][-1]) > 1){
+          locs <- strsplit(dat[2], split = "\\s+")[[1]]
+          if(length(locs != 1)){
+            locs <- strsplit(dat[2], split = ",")[[1]]
+          }
+          locs <- as.character(sapply(locs, function(x){
+            x <- strsplit(x, split = "")[[1]]
+            if(is.element(",", x)){
+              x <- x[-(which(x == ","))]
+            }
+            return(paste(x, collapse = ""))
+          }))
+          dat <- c(dat[1], locs, dat[-(1:2)])
+        }
+        
+        
         popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
         no_col <- popLoc[1] - 1
         if (popLoc[1] == 3) {
-          locs <- unlist(strsplit(dat[2], split = c("\\,", 
-                                                    "\\s+")))
+          locs <- unlist(strsplit(dat[2], split = c("\\,", "\\s+")))
           dat <- c(dat[1], locs, dat[3:length(dat)])
         }
         popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
@@ -3237,8 +3255,7 @@ pre.divLowMemory <- function(y){
             line <- paste(x, tabs, sep = "\t")
             line <- unlist(strsplit(line, split = "\t"))
             return(line)
-          }
-          else {
+          } else {
             return(x)
           }
         })
@@ -5681,14 +5698,14 @@ divBasic <- function (infile = NULL, outfile = NULL, gp = 3,
 # Master file reader
 ################################################################################
 fileReader <- function(infile){
-  if(typeof(infile)=="list"){
-    return(infile) 
-  } else if (typeof(infile)=="character"){
+  if (typeof(infile) == "list") {
+    return(infile)
+  } else if (typeof(infile) == "character") {
     flForm <- strsplit(infile, split = "\\.")[[1]]
     ext <- flForm[[length(flForm)]]
-    if(ext == "arp"){
+    if (ext == "arp") {
       convRes <- arp2gen(infile)
-      if(!is.null(convRes)){
+      if (!is.null(convRes)) {
         cat("Arlequin file converted to genepop format! \n")
         infile <- paste(flForm[1], ".gen", sep = "")
       } else {
@@ -5696,29 +5713,44 @@ fileReader <- function(infile){
       }
     }
     dat <- scan(infile, sep = "\n", what = "character", quiet = TRUE)
-    # find number of columns
+    if(length(strsplit(dat[4], split = "\\s+")[[1]][-1]) > 1){
+      locs <- strsplit(dat[2], split = "\\s+")[[1]]
+      if(length(locs != 1)){
+        locs <- strsplit(dat[2], split = ",")[[1]]
+      }
+      locs <- as.character(sapply(locs, function(x){
+        x <- strsplit(x, split = "")[[1]]
+        if(is.element(",", x)){
+          x <- x[-(which(x == ","))]
+        }
+        return(paste(x, collapse = ""))
+      }))
+      dat <- c(dat[1], locs, dat[-(1:2)])
+    }
+    
+    
     popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
     no_col <- popLoc[1] - 1
-    if(popLoc[1] == 3){
+    if (popLoc[1] == 3) {
       locs <- unlist(strsplit(dat[2], split = c("\\,", "\\s+")))
       dat <- c(dat[1], locs, dat[3:length(dat)])
     }
     popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
     no_col <- popLoc[1] - 1
-    dat1 <- sapply(dat, function(x){
+    dat1 <- sapply(dat, function(x) {
       x <- unlist(strsplit(x, split = "\\s+"))
-      if(is.element("", x)){
-        x <- x[- (which(x == ""))]
+      if (is.element("", x)) {
+        x <- x[-(which(x == ""))]
       }
-      if(is.element(",", x)){
-        x <- x[- (which(x ==","))]
+      if (is.element(",", x)) {
+        x <- x[-(which(x == ","))]
       }
-      if(length(x) != 1 && length(x) != no_col){
+      if (length(x) != 1 && length(x) != no_col) {
         x <- paste(x, collapse = "")
       }
-      if(length(x) < no_col){
-        tabs <- paste(rep(NA, (no_col - length(x))), sep = "\t", 
-                      collapse = "\t")
+      if (length(x) < no_col) {
+        tabs <- paste(rep(NA, (no_col - length(x))), 
+                      sep = "\t", collapse = "\t")
         line <- paste(x, tabs, sep = "\t")
         line <- unlist(strsplit(line, split = "\t"))
         return(line)
@@ -6040,40 +6072,59 @@ fstOnly <- function(infile = NULL, outfile = NULL, gp = 3,
     # Master file reader
     ###########################################################################
     fileReader <- function(infile){
-      if(typeof(infile)=="list"){
-        return(infile) 
-      } else if (typeof(infile)=="character"){
+      if (typeof(infile) == "list") {
+        return(infile)
+      } else if (typeof(infile) == "character") {
         flForm <- strsplit(infile, split = "\\.")[[1]]
         ext <- flForm[[length(flForm)]]
-        if(ext == "arp"){
-          arp2gen(infile)
-          cat("Arlequin file converted to genepop format! \n")
-          infile <- paste(flForm[1], ".gen", sep = "")
+        if (ext == "arp") {
+          convRes <- arp2gen(infile)
+          if (!is.null(convRes)) {
+            cat("Arlequin file converted to genepop format! \n")
+            infile <- paste(flForm[1], ".gen", sep = "")
+          } else {
+            infile <- paste(flForm[1], ".gen", sep = "")
+          }
         }
         dat <- scan(infile, sep = "\n", what = "character", quiet = TRUE)
-        # find number of columns
+        if(length(strsplit(dat[4], split = "\\s+")[[1]][-1]) > 1){
+          locs <- strsplit(dat[2], split = "\\s+")[[1]]
+          if(length(locs != 1)){
+            locs <- strsplit(dat[2], split = ",")[[1]]
+          }
+          locs <- as.character(sapply(locs, function(x){
+            x <- strsplit(x, split = "")[[1]]
+            if(is.element(",", x)){
+              x <- x[-(which(x == ","))]
+            }
+            return(paste(x, collapse = ""))
+          }))
+          dat <- c(dat[1], locs, dat[-(1:2)])
+        }
+        
+        
         popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
         no_col <- popLoc[1] - 1
-        if(popLoc[1] == 3){
+        if (popLoc[1] == 3) {
           locs <- unlist(strsplit(dat[2], split = c("\\,", "\\s+")))
-          dat <- c(dat[1], locs, dat[3:(length(dat)-3)])
+          dat <- c(dat[1], locs, dat[3:length(dat)])
         }
         popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
         no_col <- popLoc[1] - 1
-        dat1 <- sapply(dat, function(x){
+        dat1 <- sapply(dat, function(x) {
           x <- unlist(strsplit(x, split = "\\s+"))
-          if(is.element("", x)){
-            x <- x[- (which(x == ""))]
+          if (is.element("", x)) {
+            x <- x[-(which(x == ""))]
           }
-          if(is.element(",", x)){
-            x <- x[- (which(x ==","))]
+          if (is.element(",", x)) {
+            x <- x[-(which(x == ","))]
           }
-          if(length(x) != 1 && length(x) != no_col){
+          if (length(x) != 1 && length(x) != no_col) {
             x <- paste(x, collapse = "")
           }
-          if(length(x) < no_col){
-            tabs <- paste(rep(NA, (no_col - length(x))), sep = "\t", 
-                          collapse = "\t")
+          if (length(x) < no_col) {
+            tabs <- paste(rep(NA, (no_col - length(x))), 
+                          sep = "\t", collapse = "\t")
             line <- paste(x, tabs, sep = "\t")
             line <- unlist(strsplit(line, split = "\t"))
             return(line)
@@ -10301,40 +10352,59 @@ haploDiv <- function(infile = NULL, outfile = NULL, pairwise = FALSE,
   }
   haploFileReader <- function(x){
     fileReader <- function(infile){
-      if(typeof(infile)=="list"){
-        return(infile) 
-      } else if (typeof(infile)=="character"){
+      if (typeof(infile) == "list") {
+        return(infile)
+      } else if (typeof(infile) == "character") {
         flForm <- strsplit(infile, split = "\\.")[[1]]
         ext <- flForm[[length(flForm)]]
-        if(ext == "arp"){
-          arp2gen(infile)
-          cat("Arlequin file converted to genepop format! \n")
-          infile <- paste(flForm[1], ".gen", sep = "")
+        if (ext == "arp") {
+          convRes <- arp2gen(infile)
+          if (!is.null(convRes)) {
+            cat("Arlequin file converted to genepop format! \n")
+            infile <- paste(flForm[1], ".gen", sep = "")
+          } else {
+            infile <- paste(flForm[1], ".gen", sep = "")
+          }
         }
         dat <- scan(infile, sep = "\n", what = "character", quiet = TRUE)
-        # find number of columns
+        if(length(strsplit(dat[4], split = "\\s+")[[1]][-1]) > 1){
+          locs <- strsplit(dat[2], split = "\\s+")[[1]]
+          if(length(locs != 1)){
+            locs <- strsplit(dat[2], split = ",")[[1]]
+          }
+          locs <- as.character(sapply(locs, function(x){
+            x <- strsplit(x, split = "")[[1]]
+            if(is.element(",", x)){
+              x <- x[-(which(x == ","))]
+            }
+            return(paste(x, collapse = ""))
+          }))
+          dat <- c(dat[1], locs, dat[-(1:2)])
+        }
+        
+        
         popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
         no_col <- popLoc[1] - 1
-        if(popLoc[1] == 3){
+        if (popLoc[1] == 3) {
           locs <- unlist(strsplit(dat[2], split = c("\\,", "\\s+")))
-          dat <- c(dat[1], locs, dat[3:(length(dat)-3)])
+          dat <- c(dat[1], locs, dat[3:length(dat)])
         }
         popLoc <- grep("^([[:space:]]*)pop([[:space:]]*)$", tolower(dat))
         no_col <- popLoc[1] - 1
-        dat1 <- sapply(dat, function(x){
+        dat1 <- sapply(dat, function(x) {
           x <- unlist(strsplit(x, split = "\\s+"))
-          if(is.element("", x)){
-            x <- x[- (which(x == ""))]
+          if (is.element("", x)) {
+            x <- x[-(which(x == ""))]
           }
-          if(is.element(",", x)){
-            x <- x[- (which(x ==","))]
+          if (is.element(",", x)) {
+            x <- x[-(which(x == ","))]
           }
-          if(length(x) != 1 && length(x) != no_col){
+          if (length(x) != 1 && length(x) != no_col) {
             x <- paste(x, collapse = "")
           }
-          if(length(x) < no_col){
-            tabs <- paste(rep(NA, (no_col - length(x))), sep = "\t", 
-                          collapse = "\t")
+          if (length(x) < no_col) {
+            tabs <- paste(rep(NA, (no_col - length(x))), 
+                          sep = "\t", collapse = "\t")
             line <- paste(x, tabs, sep = "\t")
             line <- unlist(strsplit(line, split = "\t"))
             return(line)
