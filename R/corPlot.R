cbb384d76bf605840e2e2c3d951f9426978895f0
kkeenan02 <kkeenan02@qub.ac.uk>
2014-11-26 21:29:39 +0000

    corPlot new

--- R/corPlot.R
+++ R/corPlot.R
@@ -1,64 +1,32 @@
-################################################################################
-# corPlot, plot the relationship between divPart stats and number of alleles   #
-################################################################################
+# corPlot, plot the relationship between fastDivPart stats and number of 
+# alleles
 #' @export
-corPlot<-function(x,y){
-  x=x
-  y=y
-  par(mfrow=c(2,2))
-  par(mar=c(4,5,2,2))
-  sigStar <- function(x){
-    if(x$p.value < 0.001) {
-      return("***")
-    } else if (x$p.value < 0.01) {
-      return("**")
-    } else if (x$p.value < 0.05) {
-      return("*")
-    } else {
-      return("ns")
-    }
+corPlot <-function(infile = NULL, write = FALSE, plot.format = NULL){
+  # make sure mfrow is reset on exit
+  orig <- par()$mfrow
+  on.exit(par(mfrow = orig))
+  # calculate the number of alleles per locus
+  na <- sapply(diveRsity::rgp(infile)$af, function(x){
+    mean(colSums(x > 0), na.rm = TRUE)
+  })
+  # generate the differentiation stats
+  colselect <- c("Fst", "gst", "Gst", "D")
+  dat <- diveRsity::diffCalc(infile, fst = TRUE)$std_stats[,colselect]
+  dat <- dat[-nrow(dat),]
+  colselect <- c("Fst", "gst", "Gprimest", "D")
+  colnames(dat) <- colselect
+  dat$na <- na
+  ynms <- c(expression("F"[ST]), expression("G"[ST]), expression("G'"[ST]), 
+            expression("D"[JOST]))
+  if(write){
+    plts <- mapply(corPlotter, y = colselect, yname = ynms, 
+                   MoreArgs = list(x = "na", dat = dat,
+                                   write = TRUE, plot.format = plot.format), 
+                   SIMPLIFY = FALSE)
+    multiplot(plts[[1]], plts[[2]], plts[[3]], plts[[4]], cols = 2)
+  } else {
+    plts <- mapply(corPlotter, y = colselect, yname = ynms, 
+                   MoreArgs = list(x = "na", dat = dat), SIMPLIFY = FALSE)
+    multiplot(plts[[1]], plts[[2]], plts[[3]], plts[[4]], cols = 2)
   }
-  #Fst
-  plot(y[[2]][1:(nrow(y[[2]])-1),8]~x[[16]],
-       pch=16,xlab=expression(N[alleles]),ylab=expression(hat(theta)),
-       ylim=c(0,1),las=1)
-  abline(lm(y[[2]][1:(nrow(y[[2]])-1),8]~x[[16]]),col="red",lwd=2)
-  cor1<-cor.test(y[[2]][1:(nrow(y[[2]])-1),8],x[[16]])
-  sig <- sigStar(cor1)
-  text(x=max(x[[16]])/1.5,y=0.8,
-       labels=paste("r = ",round(cor1$estimate[[1]],3),
-                    " ", sig, sep=""),cex=2)
-  #gst
-  plot(y[[2]][1:(nrow(y[[2]])-1),4]~x[[16]],pch=16,
-       xlab=expression(N[alleles]),ylab=expression(G[st]),ylim=c(0,1),
-       las=1)
-  abline(lm(y[[2]][1:(nrow(y[[2]])-1),4]~x[[16]]),col="red",lwd=2)
-  cor2<-cor.test(y[[2]][1:(nrow(y[[2]])-1),4],x[[16]])
-  sig <- sigStar(cor2)
-  text(x=max(x[[16]])/1.5,y=0.8,
-       labels=paste("r = ",round(cor2$estimate[[1]],3),
-                    " ", sig, sep=""),cex=2)
-  #g'st
-  plot(y[[2]][1:(nrow(y[[2]])-1),5]~x[[16]],pch=16,
-       xlab=expression(N[alleles]),ylab=expression("G'"[st]),ylim=c(0,1),
-       las=1)
-  abline(lm(y[[2]][1:(nrow(y[[2]])-1),5]~x[[16]]),col="red",lwd=2)
-  cor3<-cor.test(y[[2]][1:(nrow(y[[2]])-1),5],x[[16]])
-  sig <- sigStar(cor3)
-  text(x=max(x[[16]])/1.5,y=0.8,
-       labels=paste("r = ",round(cor3$estimate[[1]],3),
-                    " ", sig, sep=""),cex=2)
-  #D
-  plot(y[[2]][1:(nrow(y[[2]])-1),6]~x[[16]],pch=16,
-       xlab=expression(N[alleles]),ylab=expression(D[est]),ylim=c(0,1),
-       las=1)
-  abline(lm(y[[2]][1:(nrow(y[[2]])-1),6]~x[[16]]),col="red",lwd=2)
-  cor4<-cor.test(y[[2]][1:(nrow(y[[2]])-1),6],x[[16]])
-  sig <- sigStar(cor4)
-  text(x=max(x[[16]])/1.5,y=0.8,
-       labels=paste("r = ",round(cor4$estimate[[1]],3),
-                    " ", sig, sep=""),cex=2)
 }
-################################################################################
-# end corPlot                                                                  #
-################################################################################
--- man/corPlot.rd
+++ man/corPlot.rd
@@ -2,30 +2,46 @@
 \alias{corPlot}
 %- Also NEED an '\alias' for EACH other topic documented here.
 \title{
-A function to plot the relationship between Gst, G'st, Theta, D (Jost) and the number of alleles at a locus.
+A function to plot the relationship between Gst, G'st, Theta, D (Jost) and the mean number of alleles at a locus. A reimplementation of \code{corPlot}. This function will replace the old function in later version of the package.
 }
 
 \description{
-\code{corPlot} uses the information calculated by \code{divPart} and \code{readGenepop} to plot and calculate the relationship between Gst, G'st, Theta, D (Jost) and the number of alleles per locus. This information can then be used to assess the likelihood that data derived from the loci is suitable for the calculation of population demography. This is based on the assumption that where \emph{u} > \emph{m}, demographic process are obscured by mutation.
+\code{corPlot} uses the information calculated by \code{diffCalc}  to plot and calculate the relationship between Gst, G'st, Theta, D (Jost) and the mean number of alleles per locus. This information can then be used to assess the likelihood that data derived from the loci are suitable for the calculation of population demography using Fst or its analogues. This is based on the assumption that where \emph{u} > \emph{m}, demographic process are obscured by mutation.
 }
 
 \details{
-This function returns an R plot for Fst (theta) vs number of alleles, Gst vs number of alleles, G'st vs number of alleles and D (Jost) vs number of alleles. 
+This function returns four scatter plots showing the relationship between Fst (Weir and Cockerham 1984), Gst (Nei and Chesser 1983), G'st (Hedrick 2005) and D (Jost 2008) and the mean number of alleles per locus. The function allows user to write plots to file, and return a four panelled figure. Plots are generated using the \code{ggplot2} package and arranged in a grid using the \code{multiplot} function by Winston Chang (Chang 2012).
 }
 
 \usage{
-corPlot(x, y)
+corPlot(infile = NULL, write = FALSE, plot.format = NULL)
 }
 
 \arguments{
 
-\item{x}{
-Results object returned from the function \code{readGenepop}
+\item{infile}{A character string indicating the location and name of a genepop format file to be read. If the file is in the current working directory, only the name must be provided. If the file is in a directory other than the current working directory, either a relative or absolute path to the file must be provided. The genepop file can be in the 2-digit or 3-digit allele format.}
+
+\item{write}{A logical argument indicating whether results should be written to file or not}
+
+\item{plot.format}{A string indicating the format to which plots should be written. Either 'png' or 'eps' are accepted.}
+
 }
 
-\item{y}{
-Results object returned from the function \code{fastDivPart}
-}
+\references{
+
+Cheng, W., R Graphics Cookbook, O'Reilly Media inc. 2012.
+
+Hedrick, P., ``A standardized genetic differentiation measure,'' Evolution,
+vol. 59, no. 8, pp. 1633-1638, (2005).
+
+Jost, L., ``G ST and its relatives do not measure differentiation,'' Molec-
+ular Ecology, vol. 17, no. 18, pp. 4015-4026, (2008).
+
+Nei, M. and Chesser, R., ``Estimation of fixation indices and gene diver-
+sities,'' Ann. Hum. Genet, vol. 47, no. Pt 3, pp. 253-259, (1983).
+
+Weir, B.S. & Cockerham, C.C., Estimating F-Statistics, for the Analysis of Population Structure, Evolution, vol. 38, No. 6, pp. 1358-1370 (1984). 
+
 }
 
 \author{
